d712c4bbd869e592053251ad217310f10e82872f
karenCLima <lima.kcls@gmail.com>
2024-10-09 18:22:20 -0300

    feat: iniciando repositorio

---
+++ src/main/java/com/back_end/JobsRocket/JobsRocketApplication.java
@@ -0,0 +1,13 @@
+package com.back_end.JobsRocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_end/JobsRocket/JobsRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_end.JobsRocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
