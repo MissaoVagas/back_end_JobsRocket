213ed4a2968dc2ef4a5083ac26eebfd7fdf671be
simplicioJoao <simpliciojoaopaulo35@gmail.com>
2024-11-11 10:36:03 -0300

    feat:JB-35-Repository: CurriculoPrincipais-CurriculoPessoais

---
+++ src/main/java/com/back_end/JobsRocket/repository/CurriculoPrincipaisRepository.java
@@ -0,0 +1,7 @@
+package com.back_end.JobsRocket.repository;
+
+import com.back_end.JobsRocket.model.CurriculoPrincipais;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurriculoPrincipaisRepository extends JpaRepository<CurriculoPrincipais, Integer> {
+}
