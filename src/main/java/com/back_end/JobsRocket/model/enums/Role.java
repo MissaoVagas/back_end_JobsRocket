632f90a7839f516d30827b4aaf0a70465b8bc964
Lucas Zanette Basso <77997005+Lucas-ZB@users.noreply.github.com>
2024-11-12 20:32:38 -0300

    Atualização-Enums.java

--- src/main/java/com/back_end/JobsRocket/model/enums/Role.java
+++ src/main/java/com/back_end/JobsRocket/model/enums/Role.java
@@ -3,3 +3,29 @@ package com.back_end.JobsRocket.model.enums;
 public enum Role {
 	CANDIDATO, RECRUTADOR
 }
+
+public enum Status {
+    ATIVA,
+    CONCLUIDA,
+    DESATIVADA
+}
+
+public enum TipoCurso {
+    GRADUACAO,
+    POS_GRADUACAO,
+    TECNICO
+}
+
+public enum TipoFormacao {
+    PRESENCIAL,
+    ONLINE,
+    HIBRIDO
+}
+
+public enum TipoEmprego {
+    CLT,
+    PJ,
+    ESTAGIO,
+    TEMPORARIO
+}
+
