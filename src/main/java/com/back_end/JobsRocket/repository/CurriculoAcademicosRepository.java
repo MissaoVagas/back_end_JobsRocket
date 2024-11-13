ee67bb67c97cca1673fa645b1269f5bc2fd42d0c
Lucas Zanette Basso <77997005+Lucas-ZB@users.noreply.github.com>
2024-11-12 21:55:42 -0300

    Criação CurriculoAcademicosRepository.java

---
+++ src/main/java/com/back_end/JobsRocket/repository/CurriculoAcademicosRepository.java
@@ -0,0 +1,27 @@
+package com.back_end.JobsRocket.repository;
+
+import com.back_end.JobsRocket.model.CurriculoAcademicos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CurriculoAcademicosRepository extends JpaRepository<CurriculoAcademicos, Integer> {
+
+    // Método para buscar por nome da faculdade
+    List<CurriculoAcademicos> findByNomeFaculdade(String nomeFaculdade);
+
+    // Método para buscar por nome do curso
+    List<CurriculoAcademicos> findByNomeCurso(String nomeCurso);
+
+    // Método para buscar por status da faculdade
+    List<CurriculoAcademicos> findByStatusFaculdade(Status statusFaculdade);
+
+    // Busca por status e tipo de curso
+    List<CurriculoAcademicos> findByStatusFaculdadeAndTipoCurso(Status statusFaculdade, TipoCurso tipoCurso);
+
+    // Busca por tipo de formação
+    List<CurriculoAcademicos> findByTipoFormacao(TipoFormacao tipoFormacao);
+    
+}
