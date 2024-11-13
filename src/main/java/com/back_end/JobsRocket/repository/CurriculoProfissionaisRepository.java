065bcb87a7a047ef4a56011db935bdb9feb4bebd
Lucas Zanette Basso <77997005+Lucas-ZB@users.noreply.github.com>
2024-11-12 21:10:33 -0300

    Criação CurriculoProfissionaisRepository.java

---
+++ src/main/java/com/back_end/JobsRocket/repository/CurriculoProfissionaisRepository.java
@@ -0,0 +1,28 @@
+package com.back_end.JobsRocket.repository;
+
+import com.back_end.JobsRocket.model.CurriculoProfissionais;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CurriculoProfissionaisRepository extends JpaRepository<CurriculoProfissionais, Integer> {
+
+    // Método para buscar currículos por nome da empresa
+    List<CurriculoProfissionais> findByNomeEmpresa(String nomeEmpresa);
+
+    // Método para buscar currículos por cargo
+    List<CurriculoProfissionais> findByCargo(String cargo);
+
+    // Busca por tipo de emprego
+    List<CurriculoProfissionais> findByTipoEmprego(TipoEmprego tipoEmprego);
+
+    // Busca por intervalo de datas
+    List<CurriculoProfissionais> findByDataInicioBetween(Date startDate, Date endDate);
+
+    // Busca por cargo e tipo de emprego
+    List<CurriculoProfissionais> findByCargoAndTipoEmprego(String cargo, TipoEmprego tipoEmprego);
+
+    
+}
