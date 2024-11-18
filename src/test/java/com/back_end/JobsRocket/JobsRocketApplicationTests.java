0b744b23188a7d06e6353d3fc7c95f240a36e72a
liziane cysne <lizianecysne1@gmail.com>
2024-11-18 18:36:55 -0300

    Adicionar perfil de teste ao JobsRocketApplicationTests

--- src/test/java/com/back_end/JobsRocket/JobsRocketApplicationTests.java
+++ src/test/java/com/back_end/JobsRocket/JobsRocketApplicationTests.java
@@ -2,12 +2,13 @@ package com.back_end.JobsRocket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JobsRocketApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
